0eb16ac36b02ab0a3763f602e4d774c0f6e17f13
William Martins <williamdabruzzmartins76136@gmail.com>
2025-11-01 17:16:13 -0300

    feat(Parser): get research projects and periodical papers

--- src/scraping/LattesParser.py
+++ src/scraping/LattesParser.py
@@ -94,6 +94,40 @@ class LattesParser:
             )
             return []
 
+
+    def _get_research_projects(self) -> List[str]:
+        research_project_link = self.soup.find("a", attrs={"name": "ProjetosPesquisa"}) 
+    
+        if research_project_link:
+            main_div_area = (
+                research_project_link.parent
+            )  # get the parent div from the ProjetosPesquisa link section
+
+            # hacky filter to filters only divs with the name 'layout-cell-pad-5' and nothing else, as observed by the website
+            # those obtain the titles
+            areas = [
+                div
+                for div in main_div_area.find_all("div")
+                if div.get("class") == ["layout-cell-pad-5"]
+            ]
+
+            area_return_list = []
+            for area in areas:
+                area_text = area.get_text(strip=True)
+                        
+
+                if not area_text.startswith("Descrição:"):
+                    print(area_text)
+                    area_return_list.append(area_text)
+
+            return area_return_list
+        else:
+            print(
+                f"Researcher from {self.filename} does not have research area sections in lattes"
+            )
+            return []
+
+
     def _get_periodicals_papers(self) -> List[str]:
         """
         Incomplete
@@ -103,10 +137,23 @@ class LattesParser:
 
         papers = periodicals_div.findAll("span", class_="transform")
 
+        titles = []
         for paper in papers:
-            print(paper.get_text(strip=True))
-            print("\n\n")
+            paper_text: str = paper.get_text(strip=True)
+            last_part: str = paper_text.split(';')[-1]
+            
+            authors_title = last_part[last_part.find("."):-1]
 
+            authors_title_split = authors_title.split(".")
+
+            for i in range(0,4):
+                if len(authors_title_split[i]) > 3:
+                    title = authors_title_split[i]
+                    break
+
+            titles.append(title)
+
+        return titles 
     def _extract_information(self) -> None:
         """Extract and store all relevant info in self.info.
 
@@ -120,6 +167,8 @@ class LattesParser:
 
         periodic = self._get_periodicals_papers()
 
+        projects = self._get_research_projects()
+
     def get_info(self) -> Dict:
         """Public getter for the extracted information from .html lattes cv
 
@@ -130,4 +179,4 @@ class LattesParser:
 
 
 if __name__ == "__main__":
-    parser = LattesParser()
+    parser = LattesParser("/home/willao/docs/Github/Auxiliar-Definicao-de-Bancas-Lattes/data/ppgcc/2740441033907310.html")
